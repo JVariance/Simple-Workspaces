20fb9cc76268a59b136f15c8751f797c37523603
JVariance <62245444+JVariance@users.noreply.github.com>
2024-01-10 14:36:51 +0100

    wip swipe overflow-container welcome screen

--- src/pages/Welcome/Welcome.svelte
+++ src/pages/Welcome/Welcome.svelte
@@ -2,22 +2,105 @@
 	import "@root/app.postcss";
 	import { onMount } from "svelte";
 	import Browser from "webextension-polyfill";
-	import { Swipe, SwipeItem } from "svelte-swipe";
-	import type { SwipeEvents, SwipeProps } from "svelte-swipe/dist/components/Swipe.svelte";
-	import type { SwipeItemEvents } from "svelte-swipe/dist/components/SwipeItem.svelte";
 
-	let swipeComp: Swipe;
 	let js_enabled = $state(false);
-	let activeView = $state(0);
-	let swipe_holder_height = $state(0);
+	let activeView = $state(1);
+	let swiping = $state(false);
+	let startSwipePos = $state({ x: 0, y: 0 });
+	let endSwipePos = $state({ x: 0, y: 0 });
+	let currentSwipePos = $state({ x: 0, y: 0 });
+	const minSwipeDistance = $state(100);
 
-	const swipeConfig: SwipeProps = {
-		transitionDuration: 500
-	};
+	function swipeStart(
+		event: PointerEvent & { currentTarget: EventTarget & HTMLDivElement }
+	) {
+		const target = event.target as HTMLElement;
 
-	function heightChanged({ detail }) {
-		console.info("height changed to " + detail.height);
-		swipe_holder_height = detail.height;
+		if (
+			!target.classList.contains("swipe-item") &&
+			!target.closest(".swipe-item")
+		)
+			return;
+		swiping = true;
+		startSwipePos.x = event.clientX;
+		startSwipePos.y = event.clientY;
+		currentSwipePos = { ...startSwipePos };
+	}
+
+	function swipeMove(
+		event: PointerEvent & { currentTarget: EventTarget & HTMLDivElement }
+	) {
+		if (swiping) {
+			const deltaPos = {
+				x: currentSwipePos.x - event.clientX,
+				y: currentSwipePos.y - event.clientY,
+			};
+			document.getElementById("wrapper")?.scrollBy({ left: deltaPos.x * 5 });
+			currentSwipePos.x = event.clientX;
+			currentSwipePos.y = event.clientY;
+		}
+	}
+
+	function swipeEnd(
+		event: PointerEvent & { currentTarget: EventTarget & HTMLDivElement }
+	) {
+		if (swiping) {
+			finishSwipe(event);
+		}
+
+		swiping = false;
+	}
+
+	function scrollViewIntoView() {
+		document.getElementById(`view-${activeView}`)?.scrollIntoView();
+	}
+
+	function cancelSwipe(
+		event: PointerEvent & {
+			target: HTMLElement;
+			currentTarget: EventTarget & HTMLDivElement;
+		}
+	) {
+		if (
+			swiping &&
+			!event.target.classList.contains("swipe-item") &&
+			!event.target.closest(".swipe-item")
+		) {
+			swiping = false;
+			finishSwipe(event);
+		}
+	}
+
+	function finishSwipe(
+		event: PointerEvent & {
+			target: HTMLElement;
+			currentTarget: EventTarget & HTMLDivElement;
+		}
+	) {
+		endSwipePos.x = event.clientX;
+		endSwipePos.y = event.clientY;
+
+		const deltaPos = {
+			x: startSwipePos.x - endSwipePos.x,
+			y: startSwipePos.y - endSwipePos.y,
+		};
+
+		const xDirection = deltaPos.x < 0 ? "left" : "right";
+
+		if (Math.abs(deltaPos.x) > minSwipeDistance) {
+			switch (xDirection) {
+				case "left":
+					activeView = Math.max(1, activeView - 1);
+					break;
+				case "right":
+					activeView = Math.min(2, activeView + 1);
+					break;
+				default:
+					break;
+			}
+		}
+
+		scrollViewIntoView();
 	}
 
 	onMount(() => {
@@ -26,87 +109,78 @@
 	});
 </script>
 
-{#snippet view1()}
-<section id="view-1" class="dark:bg-neutral-800 rounded-xl {js_enabled ? 'w-full' : 'w-[100cqw]'} p-8">
-	<h2
-		class="flex flex-wrap items-center gap-2 m-0 mb-12 text-lg first-letter:uppercase w-full justify-center"
-	>
-		<img
-			src="/icon/icon-dark.svg"
-			alt="logo"
-			width="40"
-			class="[filter:_invert()] dark:[filter:_invert(0)] w-40"
-		/>
-		<!-- <span class="basis-full w-full text-center text-2xl"
-		>Simple Workspaces</span
-	> -->
-	</h2>
-	<h1 class="text-4xl font-bold text-center mb-8">
-		Welcome to Simple Workspaces!
-	</h1>
-	<!-- <a href="about:preferences#browserRestoreSession" target="_blank"
->restore session</a
-> -->
-	You may want to enable the "open previous windows and tabs" option in preferences.
-	<button
-		class="bg-neutral-900 px-2 py-1 rounded-md"
-		onclick={(e) => {
-			window.navigator.clipboard.writeText(
-				"about:preferences#browserRestoreSession"
-			);
-			Browser.tabs.create({ active: true });
-		}}
-	>
-		copy link
-	</button>
-</section>
-{/snippet}
-{#snippet view2()}
-	<section
-		id="view-2"
-		class="dark:bg-neutral-800 rounded-xl {js_enabled ? 'w-full' : 'w-[100cqw]'} h-full p-8"
-	>
-	</section>
-{/snippet}
-
 <div class="w-full h-full p-4 @container">
-	{#if js_enabled}
-		<div class="swipe-holder w-1/2 mx-auto flex justify-center items-center" style:height="{swipe_holder_height}px">
-			<Swipe {...swipeConfig} on:change={({detail}) => {activeView = detail.active_item}} bind:this={swipeComp}>
-				<SwipeItem allow_dynamic_height={true} on:swipe_item_height_change={heightChanged}>{@render view1()}</SwipeItem>
-				<SwipeItem allow_dynamic_height={true} on:swipe_item_height_change={heightChanged}>{@render view2()}</SwipeItem>
-			</Swipe>
-		</div>
-	{:else}
-		<div
-			id="wrapper"
-			class="
-				w-1/2 h-full grid grid-cols-[100%] grid-flow-col gap-2 p-0 justify-items-center overflow-auto content-center
-				scroll-smooth snap-both snap-mandatory overscroll-x-contain @container mx-auto
-			"
+	<div
+		id="wrapper"
+		class:swiping
+		class="
+			w-1/2 h-full grid grid-cols-[100%] grid-flow-col gap-2 p-0 justify-items-center overflow-auto content-center
+			scroll-smooth snap-both snap-mandatory overscroll-x-contain @container mx-auto
+			[&.swiping]:cursor-grabbing
+			[&.swiping]:select-none
+		"
+		on:pointerdown={swipeStart}
+		on:pointermove={swipeMove}
+		on:pointerup={swipeEnd}
+		on:pointerleave={cancelSwipe}
+	>
+		<!-- on:pointerleave={cancelSwipe} -->
+		<section
+			id="view-1"
+			class="swipe-item dark:bg-neutral-800 rounded-xl w-[100cqw] p-8"
 		>
-			{@render view1()}
-			{@render view2()}
-		</div>
-	{/if}
+			<h2
+				class="flex flex-wrap items-center gap-2 m-0 mb-12 text-lg first-letter:uppercase w-full justify-center"
+			>
+				<img
+					src="/icon/icon-dark.svg"
+					alt="logo"
+					width="40"
+					class="[filter:_invert()] dark:[filter:_invert(0)] w-40"
+					draggable="false"
+				/>
+				<!-- <span class="basis-full w-full text-center text-2xl"
+				>Simple Workspaces</span
+			> -->
+			</h2>
+			<h1 class="text-4xl font-bold text-center mb-8">
+				Welcome to Simple Workspaces!
+			</h1>
+			<!-- <a href="about:preferences#browserRestoreSession" target="_blank"
+		>restore session</a
+	> -->
+			You may want to enable the "open previous windows and tabs" option in preferences.
+			<button
+				class="bg-neutral-900 px-2 py-1 rounded-md"
+				onclick={(e) => {
+					window.navigator.clipboard.writeText(
+						"about:preferences#browserRestoreSession"
+					);
+					Browser.tabs.create({ active: true });
+				}}
+			>
+				copy link
+			</button>
+		</section>
+		<section
+			id="view-2"
+			class="swipe-item dark:bg-neutral-800 rounded-xl w-[100cqw] h-full p-8"
+		></section>
+	</div>
 	<div
 		id="view-buttons"
 		class="fixed bottom-8 left-1/2 -translate-x-1/2 flex gap-2"
 	>
 		{#each Array(2) as _, i}
 			<button
-				class:active={activeView === i}
+				class:active={activeView === i + 1}
 				class="rounded-full w-3 h-3 [&.active]:bg-[#fde9ff]"
 				onclick={() => {
-					if(js_enabled) {
-						activeView = i;
-						swipeComp.goTo(activeView);
-					} else {
-						activeView = i + 1;
-						document.getElementById(`view-${i + 1}`)?.scrollIntoView();
-					}
+					activeView = i + 1;
+					scrollViewIntoView();
 				}}
-			/>
+			>
+			</button>
 		{/each}
 	</div>
 </div>
