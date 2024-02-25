e707982d8a6cfb6754be1b3a7552fe452823b041
JVariance <62245444+JVariance@users.noreply.github.com>
2024-02-25 16:25:07 +0100

    wip adjust options page to new welcome page design

--- extension/src/pages/Options/Options.svelte
+++ extension/src/pages/Options/Options.svelte
@@ -57,139 +57,235 @@
 </script>
 
 {#snippet Section(content, classes)}
-	<section class="p-2 border border-solid rounded-md border-gray-100 dark:border-neutral-800 bg-neutral-50/30 dark:bg-[#23222b] w-[100cqw] overflow-auto sm:overflow-visible {classes}">
+	<section class="[background:_var(--section-bg)] p-4 rounded-md grid gap-4 basis-full max-w-[100cqw] overflow-auto md:overflow-visible {classes}">
 		{@render content()}
 	</section>
 {/snippet}
 
 <Layout>
-	<div class="p-8">
-		<div class="flex items-center gap-2 mb-4 justify-between">
-			<h2 class="flex items-center gap-2 m-0 text-lg first-letter:uppercase">
-				<Logo />
-				Simple Workspaces
-			</h2>
-			<p class="text-black/75 dark:text-white/75">{i18n.getMessage('version')}: {Browser.runtime.getManifest().version}</p>
+	<header class="flex h-16 sticky top-0 z-10 items-center mb-24 justify-center">
+		<div
+			class="
+			flex flex-wrap items-center h-max p-2 justify-between
+		bg-[--body-bg] rounded-full
+			w-full sm:w-[500px] md:w-[700px] lg:w-[900px] xl:w-[1100px] 2xl:w-[1300px]
+		"
+		>
+			<h1
+				class="text-base animate-fade-left animate-duration-1000 flex gap-2 bg-[--badge-bg] rounded-full items-center py-2 px-4 text-[--heading-2-color]"
+			>
+				<Logo class="w-6 h-6" draggable="false" />
+				<span class="text-base md:text-xl">Simple Workspaces</span>
+			</h1>
+			<p
+				class="py-2 px-4 rounded-full text-base md:text-xl bg-[--badge-bg] text-[--heading-2-color]"
+			>
+				v{Browser.runtime.getManifest().version}
+			</p>
 		</div>
+	</header>
+	<main
+		class="
+			flex flex-wrap gap-8 w-3/4 sm:w-[400px] md:w-[600px] lg:w-[800px] xl:w-[1000px] 2xl:w-[1200px] mx-auto mb-8
+			@container
+		"
+	>
+		{#snippet Section_Theme()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color] first-letter:uppercase">{i18n.getMessage('theme')}</h1>
+			<ThemeSwitch />
+		{/snippet}
+		{#snippet Section_HomeWorkspace()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color] first-letter:uppercase">{i18n.getMessage('home_workspace')}</h1>
+			<HomeWorkspace />
+		{/snippet}
+		{#snippet Section_CurrentWorkspaces()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color] first-letter:uppercase">{i18n.getMessage('current_workspaces')}</h1>
+			<ul class="current-workspaces grid gap-4">
+				{#each windowWorkspaces as workspace}
+					<li class="flex items-stretch gap-2">
+						<SimpleWorkspace 
+							{workspace} 
+							updatedName={(name) => {workspace.name = name;}}
+							updatedIcon={(icon) => {workspace.icon = icon;}}
+						/>
+					</li>
+				{/each}
+			</ul>
 
-		<!-- <h1 class="first-letter:uppercase mb-2">{i18n.getMessage('options')}</h1> -->
-
-		<div class="flex flex-wrap gap-4 mt-16 flex-col sm:flex-row @container">
-			<!-- <section>
-				<h2 class="m-0 mb-4 text-lg first-letter:uppercase">🌍 {i18n.getMessage('language')}</h2>
-				<select id="selectLanguage">
-					{#each ['en', 'de-DE'] as lang}
-						<option value={lang}>{lang}</option>
-					{/each}
-				</select>
-			</section> -->
-			{#snippet Section_Theme()}
-				<h2 class="m-0 mb-4 text-lg font-semibold first-letter:uppercase">{i18n.getMessage('theme')}</h2>
-				<ThemeSwitch />
-			{/snippet}
-			{#snippet Section_HomeWorkspace()}
-				<h2 class="m-0 mb-4 text-lg font-semibold first-letter:uppercase">{i18n.getMessage('home_workspace')}</h2>
-				<HomeWorkspace />
-			{/snippet}
-			{#snippet Section_CurrentWorkspaces()}
-				<h2 class="m-0 mb-4 text-lg font-semibold first-letter:uppercase">{i18n.getMessage('current_workspaces')}</h2>
-				<ul class="current-workspaces grid gap-4">
-					{#each windowWorkspaces as workspace}
-						<li class="flex items-stretch gap-2">
-							<SimpleWorkspace 
-								{workspace} 
-								updatedName={(name) => {workspace.name = name;}}
-								updatedIcon={(icon) => {workspace.icon = icon;}}
-							/>
-						</li>
-					{/each}
-				</ul>
-
-				{#if windowWorkspaces?.length}
-					<button class="btn justify-center mt-4" style:width="-moz-available" onclick={applyCurrentWorkspacesChanges}>
-						<Icon icon="check" />
-						<span class="-mt-1">{i18n.getMessage('apply_changes')}</span>
-					</button>
-					{:else}
-					{i18n.getMessage('There_are_no_current_workspaces_in_this_window')}.
-				{/if}
-			{/snippet}
-			{#snippet Section_DefaultWorkspaces()}
-				<h2 class="m-0 mb-4 text-lg font-semibold first-letter:uppercase">{i18n.getMessage('default_workspaces')}</h2>
-				<Info class="mb-4">
-					{i18n.getMessage('changes_will_apply_for_new_windows')}
-				</Info>
-				<DefaultWorkspaces />
-			{/snippet}
-			{#snippet Section_TabPinning()}
-				<h2 class="m-0 mb-4 text-lg font-semibold first-letter:uppercase">{i18n.getMessage('tab_pinning')}</h2>
-				<div class="flex gap-4 items-center w-full">
-					<input
-						type="checkbox"
-						id="keep-pinned-tabs"
-						checked={keepPinnedTabs}
-						onchange={keepPinnedTabsChanged}
-					/>
-					<label for="keep-pinned-tabs" class="-mt-[0.2rem]">{i18n.getMessage("keep_pinned_tabs")}</label>
-				</div>
-			{/snippet}
-			{#snippet Section_Shortcuts()}
-				<h2 class="m-0 mb-4 text-lg flex gap-2 items-center font-semibold">
-					<span class="first-letter:uppercase">{i18n.getMessage('shortcuts')}</span>
-				</h2>
-				<Info>
-					<Tooltip id="shortcuts-info" class="[&_svg]:w-4 [&_svg]:h-4" popupClasses="absolute top-0 right-0 w-max">
-						{@html i18n.getMessage('you_can_edit_shortcuts_for_commands_in_the_addons_page')}
-						{#snippet message()}
-							<ButtonLink href={i18n.getMessage('shortcuts.help.link')} target="_blank" class="ghost flex flex-nowrap items-center gap-1">
-								{i18n.getMessage('read_more')}
-							</ButtonLink>
-						{/snippet}
-					</Tooltip>
-				</Info>
-				<Shortcuts />
-			{/snippet}
-			{#snippet Section_ClearExtensionData()}
-				<Accordion class="border-none">
-					{#snippet summary()}
-						<Summary class="border-none">
-							<h2 class="m-0 text-lg flex gap-2 items-center font-semibold">
-								<Icon icon="clear" />
-								<span class="-mt-[0rem] first-letter:uppercase">{i18n.getMessage('clear')}</span>
-							</h2>
-						</Summary>
+			{#if windowWorkspaces?.length}
+				<button class="btn justify-center mt-4" style:width="-moz-available" onclick={applyCurrentWorkspacesChanges}>
+					<Icon icon="check" />
+					<span class="-mt-1">{i18n.getMessage('apply_changes')}</span>
+				</button>
+				{:else}
+				{i18n.getMessage('There_are_no_current_workspaces_in_this_window')}.
+			{/if}
+		{/snippet}
+		{#snippet Section_DefaultWorkspaces()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color] first-letter:uppercase">{i18n.getMessage('default_workspaces')}</h1>
+			<Info class="mb-4">
+				{i18n.getMessage('changes_will_apply_for_new_windows')}
+			</Info>
+			<DefaultWorkspaces />
+		{/snippet}
+		{#snippet Section_TabPinning()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color] first-letter:uppercase">{i18n.getMessage('tab_pinning')}</h1>
+			<div class="flex gap-4 items-center w-full">
+				<input
+					type="checkbox"
+					id="keep-pinned-tabs"
+					checked={keepPinnedTabs}
+					onchange={keepPinnedTabsChanged}
+				/>
+				<label for="keep-pinned-tabs" class="-mt-[0.2rem]">{i18n.getMessage("keep_pinned_tabs")}</label>
+			</div>
+		{/snippet}
+		{#snippet Section_Shortcuts()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color]">
+				<span class="first-letter:uppercase">{i18n.getMessage('shortcuts')}</span>
+			</h1>
+			<Info>
+				<Tooltip id="shortcuts-info" class="[&_svg]:w-4 [&_svg]:h-4" popupClasses="absolute top-0 right-0 w-max">
+					{@html i18n.getMessage('you_can_edit_shortcuts_for_commands_in_the_addons_page')}
+					{#snippet message()}
+						<ButtonLink href={i18n.getMessage('shortcuts.help.link')} target="_blank" class="ghost flex flex-nowrap items-center gap-1">
+							{i18n.getMessage('read_more')}
+						</ButtonLink>
 					{/snippet}
-					<button class="btn mt-4" onclick={clearExtensionData}>{i18n.getMessage('clear')}</button>
-				</Accordion>
-			{/snippet}
-			{#snippet Section_WelcomePage()}
-				<h2 class="m-0 mb-4 text-lg flex gap-2 items-center font-semibold first-letter:uppercase">{i18n.getMessage('want_to_see_the_welcome_page_again')}</h2>
-				<ButtonLink class="btn w-max" href="{Browser.runtime.getURL('src/pages/Welcome/welcome.html')}" target="_blank">{i18n.getMessage('open_welcome_page')}</ButtonLink>
-			{/snippet}
-			{#snippet Section_FurtherLinks()}
-				<h2 class="m-0 mb-4 text-lg flex gap-2 items-center font-semibold first-letter:uppercase">{i18n.getMessage("feedback_and_feature_requests_heading")}</h2>
-				<ButtonLink href="https://github.com/JVariance/Simple-Workspaces" target="_blank" class="btn w-max">
-					<img src="/images/github-mark/github-mark-white.svg" alt="GitHub Logo" class="w-6 aspect-square [@media_(prefers-color-scheme:_light)]:hidden">
-					<img src="/images/github-mark/github-mark.svg" alt="GitHub Logo" class="w-6 aspect-square dark:hidden">
-					{i18n.getMessage('github_repository')}
-				</ButtonLink>
-			{/snippet}
+				</Tooltip>
+			</Info>
+			<Shortcuts />
+		{/snippet}
+		{#snippet Section_ClearExtensionData()}
+			<Accordion class="border-none">
+				{#snippet summary()}
+					<Summary class="border-none">
+						<h1 class="text-xl font-semibold text-[--heading-2-color] flex gap-2 items-center">
+							<Icon icon="clear" />
+							<span class="-mt-[0rem] first-letter:uppercase">{i18n.getMessage('clear')}</span>
+						</h1>
+					</Summary>
+				{/snippet}
+				<button class="btn mt-4" onclick={clearExtensionData}>{i18n.getMessage('clear')}</button>
+			</Accordion>
+		{/snippet}
+		{#snippet Section_WelcomePage()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color]">{i18n.getMessage('want_to_see_the_welcome_page_again')}</h1>
+			<ButtonLink class="btn w-max" href="{Browser.runtime.getURL('src/pages/Welcome/welcome.html')}" target="_blank">{i18n.getMessage('open_welcome_page')}</ButtonLink>
+		{/snippet}
+		{#snippet Section_FurtherLinks()}
+			<h1 class="text-xl font-semibold text-[--heading-2-color]">{i18n.getMessage("feedback_and_feature_requests_heading")}</h1>
+			<ButtonLink href="https://github.com/JVariance/Simple-Workspaces" target="_blank" class="btn w-max">
+				<img src="/images/github-mark/github-mark-white.svg" alt="GitHub Logo" class="w-6 aspect-square [@media_(prefers-color-scheme:_light)]:hidden">
+				<img src="/images/github-mark/github-mark.svg" alt="GitHub Logo" class="w-6 aspect-square dark:hidden">
+				{i18n.getMessage('github_repository')}
+			</ButtonLink>
+		{/snippet}
 
-			{@render Section(Section_Theme, "basis-full flex-1")}
-			{@render Section(Section_HomeWorkspace, "flex-1")}
-			{@render Section(Section_CurrentWorkspaces, "flex-1")}
-			{@render Section(Section_DefaultWorkspaces, "flex-1 overflow-auto scrollbar-gutter:_stable] sm:scrollbar-gutter:_unset]")}
-			{@render Section(Section_TabPinning, "basis-full")}
-			{@render Section(Section_Shortcuts, "basis-full")}
-			{@render Section(Section_ClearExtensionData, "basis-full")}
-			{@render Section(Section_WelcomePage, "flex-1")}
-			{@render Section(Section_FurtherLinks, "flex-1")}
-		</div>
-	</div>
+		{@render Section(Section_Theme, "basis-full flex-1")}
+		{@render Section(Section_HomeWorkspace, "flex-1")}
+		{@render Section(Section_CurrentWorkspaces, "flex-1")}
+		{@render Section(Section_DefaultWorkspaces, "flex-1 overflow-auto scrollbar-gutter:_stable] sm:scrollbar-gutter:_unset]")}
+		{@render Section(Section_TabPinning, "basis-full")}
+		{@render Section(Section_Shortcuts, "basis-full")}
+		{@render Section(Section_ClearExtensionData, "basis-full")}
+		{@render Section(Section_WelcomePage, "flex-1")}
+		{@render Section(Section_FurtherLinks, "flex-1")}
+	</main>
 </Layout>
 
 <style lang="postcss">
-	:global(html) {
+
+	:root {
+		--body-bg: light-dark(#ffffff, #181b3e);
+		--section-bg: linear-gradient(
+			to bottom,
+			light-dark(#f6f6ff, #261f4a),
+			light-dark(#f1f1ff, #261f4a)
+		);
+		--section-color: light-dark(#32304e, #d1cfe8);
+		--workspace-bg: light-dark(#ebeafe, #342c59);
+		--workspace-color: light-dark(#625eb7, #c6c4f0);
+		--workspace-handle-fill: #43407d;
+		--workspace-remove-fill: #43407d;
+		--button-primary-bg: #625eb7;
+		--button-primary-hover-bg: hsl(243, 38%, 44%);
+		--button-primary-color: #ffffff;
+		--button-primary-hover-color: #ffffff;
+		--button-secondary-bg: #b5b3e8;
+		--button-secondary-color: #ffffff;
+		--kbd-bg: light-dark(#dddcfa, #43407d);
+		--kbd-color: light-dark(#43407d, #aba7f7);
+		--badge-bg: light-dark(#f8f8fe, #282853);
+		--heading-color: #8c88e3;
+		--heading-2-color: light-dark(#625eb7, #aca8f9);
+	}
+
+	:global(body) {
+		@apply bg-[--body-bg];
 		color-scheme: light dark;
+	}
+
+	:global(.btn) {
+		@apply bg-[--button-primary-bg] text-[--button-primary-color] rounded-full py-2 px-4 flex gap-2 items-center w-max border-none;
+		@apply hover:bg-[--button-primary-hover-bg] hover:text-[--button-primary-hover-color];
+		@apply focus:bg-[--button-primary-hover-bg] hover:text-[--button-primary-hover-color];
+		@apply focus-visible:bg-[--button-primary-hover-bg] hover:text-[--button-primary-hover-color];
+	}
+
+	:global(.btn.secondary-btn) {
+		@apply bg-[--workspace-bg] text-[--workspace-color] rounded-md;
+	}
+
+	:global(input[type="text"]) {
+		@apply !bg-[--workspace-bg] text-[--workspace-color] !border-none;
+	}
+
+	:global(.drag-handle) {
+		@apply text-[--workspace-handle-fill];
+	}
+
+	:global(kbd) {
+		@apply shadow-none bg-[--kbd-bg] text-[--kbd-color] border-none p-1;
+	}
+
+	:global(dl) {
+		@apply text-[--section-color] gap-px;
+	}
+
+	:global(dl, dl > div, dd) {
+		@apply border-none;
+	}
+
+	:global(dl > div) {
+		@apply !gap-px;
+	}
+
+	:global(dl, dl > div, dd) {
+		@apply !border-[--kbd-bg];
+	}
+
+	:global(dt) {
+		@apply bg-[light-dark(#eaeafc,_#342c59)] py-1 px-2;
+	}
+
+	:global(dd) {
+		@apply bg-[light-dark(#eaeafc,_#342c59)];
+	}
+
+	:global(dl > div:first-of-type dt) {
+		@apply rounded-tl-[3px];
+	}
+
+	:global(dl > div:first-of-type dd) {
+		@apply rounded-tr-[3px];
+	}
+
+	:global(dl > div:last-of-type dt) {
+		@apply rounded-bl-[3px];
+	}
+
+	:global(dl > div:last-of-type dd) {
+		@apply rounded-br-[3px];
 	}
 </style>
